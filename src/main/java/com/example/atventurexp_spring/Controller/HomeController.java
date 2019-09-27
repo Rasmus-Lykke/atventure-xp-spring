6553b3925516012a1bc66e3fdd4c3ae21b377ae6
MikkelBeier <MikkelBeier@hotmail.com>
2019-09-27 13:06:37 +0200

    Start på home.html + bookevent.hmtl

--- src/main/java/com/example/atventurexp_spring/Controller/HomeController.java
+++ src/main/java/com/example/atventurexp_spring/Controller/HomeController.java
@@ -1,4 +1,12 @@
 package com.example.atventurexp_spring.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String root() {
+        return "Home.html";
+    }
 }
