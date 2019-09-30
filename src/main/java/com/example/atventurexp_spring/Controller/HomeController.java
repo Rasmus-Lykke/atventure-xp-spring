8b45b196aa542af04fd30f8fd088417e07a189c7
Johannes <themeatpi@gmail.com>
2019-09-30 11:12:07 +0200

    added BookingOversigt HTML

--- src/main/java/com/example/atventurexp_spring/Controller/HomeController.java
+++ src/main/java/com/example/atventurexp_spring/Controller/HomeController.java
@@ -1,4 +1,18 @@
 package com.example.atventurexp_spring.Controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 public class HomeController {
+    @GetMapping("/")
+    public String Home(Model model){
+        return "Home.html";
+    }
+
+    @GetMapping("/BookingOversigt")
+    public String BookingOversigt(Model model){
+        return "BookingOversigt.html";
+    }
 }
