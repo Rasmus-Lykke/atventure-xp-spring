c330a1819c02ba212c64a46048929dfb50b851ce
Knud Billing <knudjbilling@gmail.com>
2019-09-30 12:12:26 +0200

    Tilføjet HTML side for at vise udstyrs oversigt for en booking

--- src/main/java/com/example/atventurexp_spring/Controller/HomeController.java
+++ src/main/java/com/example/atventurexp_spring/Controller/HomeController.java
@@ -1,10 +1,12 @@
 package com.example.atventurexp_spring.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+@Controller
 public class HomeController {
     @GetMapping("/")
     public String Home(Model model){
