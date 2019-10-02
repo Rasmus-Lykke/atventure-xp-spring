08caf1ac0aed7bcd01b19042c21c5c8e18cb97b4
MikkelBeier <MikkelBeier@hotmail.com>
2019-10-02 13:26:20 +0200

    Deleted SQL mappe

--- SQL/DML.sql
+++
@@ -1,9 +0,0 @@
-
--- Select the database
-USE AtventureDB;
-
-INSERT INTO `atventuredb`.`activities` (`activityName`, `ageRestriction`, `timeLimit` )
-VALUES ('Go-Kart', '17', '16');
-
-
-SELECT * FROM atventuredb.activities;
