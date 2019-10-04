6e4f68da48300901769f23fd5aacbb3d5908e420
Lise-Lu <lise2108@stud.kea.dk>
2019-10-04 13:55:57 +0200

    ADDED manual booking to booking table

--- MySQL/DML.sql
+++ MySQL/DML.sql
@@ -8,3 +8,8 @@ VALUES ('Go-Kart',"This is a test restriction",1);
 INSERT INTO atventuredb.equipment(equipmentID, activityID, equipment_name, equipment_amount)
 VALUES (1,1,'Hjelm',3);
 
+INSERT INTO atventuredb.booking(bookingID, activityID, participants, date, time, contact_name, contact_phone, contact_email, instructor, corporate)
+VALUES (1, 1, 4,"2019-09-09","08:00","Jason","12345678","jason@derulo.com", "Peter", "false");
+
+
+
