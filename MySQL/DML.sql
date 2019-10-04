77e191fd21e47a41c344dba90359f330c78633ab
Knud Billing <knudjbilling@gmail.com>
2019-10-04 14:24:27 +0200

    Changed restriction text

--- MySQL/DML.sql
+++ MySQL/DML.sql
@@ -3,7 +3,7 @@
 USE AtventureDB;
 
 INSERT INTO `atventuredb`.`activity` (`activity_name`,`restrictions`, activityID)
-VALUES ('Go-Kart','This is a test restriction',1);
+VALUES ('Go-Kart','Skal være over 150cm høj og under 220cm høj',1);
 
 INSERT INTO atventuredb.equipment(equipmentID, activityID, equipment_name, equipment_amount)
 VALUES (1,1,'Hjelm',3),
