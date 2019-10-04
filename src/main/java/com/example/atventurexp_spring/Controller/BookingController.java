4218dea2d24bd908a4cd6349d91be7fa78426e23
Bad-Snowman <43063683+Bad-Snowman@users.noreply.github.com>
2019-10-04 14:31:14 +0200

    UpdateBooking.html

--- src/main/java/com/example/atventurexp_spring/Controller/BookingController.java
+++ src/main/java/com/example/atventurexp_spring/Controller/BookingController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -26,7 +27,6 @@ public class BookingController {
         List<Activity> activityList = activityService.getAllActivities();
         model.addAttribute("activityList", activityList);
         return "BookEvent.html";
-
     }
 
     @PostMapping("/BookEvent")
@@ -36,10 +36,39 @@ public class BookingController {
     }
 
 
+
+
     @PostMapping("/UpdateBooking")
     public String updateBooking(@ModelAttribute Booking booking) {
         bookingService.updateByID(booking);
         return "redirect:/";
     }
+
+    @GetMapping("/UpdateBooking")
+    public String updateBooking(){
+        return "UpdateBooking.html";
+    }
+
+
+
+    @GetMapping("/UpdateBookingByID/{bookingID}")
+    public String updateBookingByID(@PathVariable("bookingID") long bookingID) {
+        bookingService.getById(bookingID);
+        return "UpdateBookingByID.html";
+    }
+
+    @GetMapping("/UpdateBookingByID")
+    public String updateBookingByID(Long bookingID) {
+        bookingService.getById(bookingID);
+        return "UpdateBookingByID.html";
+    }
+
+
+    @PostMapping("/SaveUpdateBookingByID")
+    public String saveUpdateBookingByID(@ModelAttribute Booking booking) {
+        bookingService.updateByID(booking);
+        return "redirect:/";
+    }
+
 
 }
