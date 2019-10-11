613543db44a9029c1eb0be7f83ee17d100b70dfe
Knud Billing <knudjbilling@gmail.com>
2019-10-11 14:43:08 +0200

    Diverse rettelser

--- MySQL/DML.sql
+++ MySQL/DML.sql
@@ -12,5 +12,8 @@ VALUES (1,1,'Hjelm',3),
 INSERT INTO AtventureDB.booking(bookingID, activityID, participants, date, time, contact_name, contact_phone, contact_email, instructor, corporate)
 VALUES (1, 1, 4,"2019-09-09","08:00","Jason","12345678","jason@derulo.com", "Peter", "false");
 
+INSERT INTO AtventureDB.booking(bookingID, activityID, participants, date, time, contact_name, contact_phone, contact_email, instructor, corporate)
+VALUES (2, 1, 7,"2020-10-11","09:15","Vladimir","987658","vlad@mail.com", "Igor", "true");
+
 
 
